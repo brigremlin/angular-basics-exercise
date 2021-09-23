f50723f6def1e260f93d8037a6bdcb1bc034f71b
brigremlin <brigrimsley@gmail.com>
2021-09-23 17:24:13 -0500

    bg: Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule
